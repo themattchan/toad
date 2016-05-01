1b31d2b396ae6a9eeadac7ddaf9e8d73c3e9cfe5
Matthew Chan <matt@themattchan.com>
2016-04-30 18:10:47 -0700

    yaml

---
+++ info.rkt
@@ -0,0 +1,15 @@
+#lang setup/infotab
+
+(define version "0.1")
+(define collection 'multi)
+(define deps '("base"
+               "html-lib"
+               "racket-index"
+               "scribble-lib"
+               "srfi-lite-lib"
+               "web-server-lib"
+               ("markdown" "0.15")
+               ("rackjure" "0.7")
+               "find-parent-dir"))
+(define build-deps '("at-exp-lib"
+                     "rackunit-lib"))
